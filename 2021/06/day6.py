4df3122f247125857408d958137b62a6950cdf96
honswlos <25736725+honswlos@users.noreply.github.com>
2021-12-06 20:54:29 -0800

    Day 6

--- 2021/06/day6.py
+++ 2021/06/day6.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
+from collections import Counter
 
 print("AoC 2021 Day 6")
 
-with open("testinput", "rt") as f:
+with open("input", "rt") as f:
     lanternfish_ages = [int(age) for age in f.read().split(",")]
 
-# Part 1
-days = 80
+# Parts 1 & 2
+days = 256
+
+fish = Counter(lanternfish_ages)
 
 for day in range(days + 1):
-    print(f"day {day}")
-    new_fish = lanternfish_ages.count(-1)
-    # for _ in range(new_fish):
-    #     lanternfish_ages.remove(-1)
-    # lanternfish_ages[lanternfish_ages.index(-1)] = 6
-    lanternfish_ages = [f for f in lanternfish_ages if f != -1]
-    print("done removing old fish")
-    lanternfish_ages += [6] * new_fish
-    lanternfish_ages += [8] * new_fish
-    print("done spawning new fish")
-    lanternfish_ages = [age - 1 for age in lanternfish_ages]
-    print("done creating new fish list")
+    try:
+        new_fish = fish.pop(-1)
+        fish.update(Counter({6: new_fish, 8: new_fish}))
+    except KeyError:
+        pass
+    fish = Counter({age - 1: count for age, count in fish.items()})
+    if day == 80:
+        print(f"Part 1: {sum(fish.values())}")
 
-print(f"Part 1: {len(lanternfish_ages)}")
+print(f"Part 2: {sum(fish.values())}")
